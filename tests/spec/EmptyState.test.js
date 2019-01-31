93fc5b1544f25f7816c51e1f67212f32412ff0bd
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-01-30 19:32:41 -0500

    added test to EmptyState (#23)

--- tests/spec/EmptyState.test.js
+++ tests/spec/EmptyState.test.js
@@ -1,11 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-testing-library";
 import { EmptyState } from "../../src/index";
 
 describe("EmptyState", () => {
   test("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<EmptyState title="Empty State" />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    render(<EmptyState />);
+  });
+
+  test("props work", () => {
+    render(<EmptyState className="test" icon="check" />);
   });
 });
