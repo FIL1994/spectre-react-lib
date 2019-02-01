6a87c8baf61962614adc0e59a9fa3afd10d6d4d4
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-01-31 22:00:27 -0500

    added tests for Panel and Pagination

--- tests/spec/Pagination.test.js
+++ tests/spec/Pagination.test.js
@@ -1,11 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render, fireEvent } from "react-testing-library";
 import { Pagination } from "../../src/index";
 
 describe("Pagination", () => {
   test("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<Pagination onClick={() => {}} totalPages={3} />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    render(<Pagination onClick={() => {}} totalPages={3} />);
+  });
+
+  test("calls onClick", () => {
+    const onClickMock = jest.fn(() => {});
+
+    const { getByText } = render(
+      <Pagination onClick={onClickMock} totalPages={10} activePage={2} />
+    );
+
+    const page1 = getByText("1");
+    fireEvent.click(page1);
+
+    expect(onClickMock).toHaveBeenCalled();
   });
 });
