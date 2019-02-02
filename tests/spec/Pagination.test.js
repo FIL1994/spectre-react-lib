0c88a6b488de72664f3af8973c1200b4201a34a8
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-02-01 23:12:52 -0500

    added tests for Pagination (#26)

--- tests/spec/Pagination.test.js
+++ tests/spec/Pagination.test.js
@@ -10,13 +10,32 @@ describe("Pagination", () => {
   test("calls onClick", () => {
     const onClickMock = jest.fn(() => {});
 
-    const { getByText } = render(
-      <Pagination onClick={onClickMock} totalPages={10} activePage={2} />
+    const { getByText, container } = render(
+      <Pagination
+        onClick={onClickMock}
+        totalPages={10}
+        activePage={2}
+        centered
+      />
     );
 
-    const page1 = getByText("1");
+    const page1 = getByText("4");
     fireEvent.click(page1);
 
+    fireEvent.click(container.querySelector(".page-item:last-of-type"));
+    fireEvent.click(container.querySelector(".page-item:first-of-type"));
+
     expect(onClickMock).toHaveBeenCalled();
+  });
+
+  test("handles one page", () => {
+    const { getByText, container } = render(
+      <Pagination onClick={() => {}} totalPages={1} centered />
+    );
+
+    getByText("1");
+
+    const pageItems = container.querySelectorAll(".page-item");
+    expect(pageItems).toHaveLength(3);
   });
 });
