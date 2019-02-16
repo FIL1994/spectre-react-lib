fa69b94f3582eea977b0d248734d5787d3437433
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-02-16 11:14:39 -0500

    added type definitions to Toast

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,5 +2,6 @@ import Divider from "./utilities/Divider";
 import Loading from "./utilities/Loading";
 import Grid from "./layout/Grid";
 import Parallax from "./experimentals/Parallax";
+import Toast from "./components/Toast";
 
-export { Divider, Loading, Grid, Parallax };
+export { Divider, Loading, Grid, Parallax, Toast };
