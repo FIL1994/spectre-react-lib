d517d425e44c259f3aea3a3d3130523db6f7cf60
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-02-16 11:20:58 -0500

    added type definitions to Panel (#39)

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,5 +3,6 @@ import Loading from "./utilities/Loading";
 import Grid from "./layout/Grid";
 import Parallax from "./experimentals/Parallax";
 import Toast from "./components/Toast";
+import Panel from "./components/Panel";
 
-export { Divider, Loading, Grid, Parallax, Toast };
+export { Divider, Loading, Grid, Parallax, Toast, Panel };
