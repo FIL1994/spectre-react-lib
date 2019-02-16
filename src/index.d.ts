42d989d6e5d515a176bf17c6c3afd04b6ba902d7
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-02-16 11:05:40 -0500

    added type definitions for Parallax (#37)

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,6 @@
 import Divider from "./utilities/Divider";
 import Loading from "./utilities/Loading";
 import Grid from "./layout/Grid";
+import Parallax from "./experimentals/Parallax";
 
-export { Divider, Loading, Grid };
+export { Divider, Loading, Grid, Parallax };
