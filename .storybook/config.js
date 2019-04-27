c071735c69aec034f89e3685faefeb10d8df8d63
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-04-27 16:37:18 -0400

    added Divider story

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,12 @@
-import { configure } from '@storybook/react';
+import { configure, setAddon } from "@storybook/react";
+import infoAddon from "@storybook/addon-info";
+
+setAddon(infoAddon);
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /\.stories\.js$/);
+const req = require.context("../stories", true, /\.stories\.js$/);
 function loadStories() {
+  require("../stories/index.stories");
   req.keys().forEach(filename => req(filename));
 }
 
---
+++ stories/Divider.stories.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+import "spectre.css";
+import { Divider } from "../src";
+import { withInfo } from "@storybook/addon-info";
+
+storiesOf("Divider", module)
+  .addDecorator(withInfo)
+  .add("basic", () => (
+    <div style={{ margin: 100 }}>
+      <Divider />
+    </div>
+  ));
