10ebc9b8d00d76d998c4ea140916405e60c00dc9
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-04-29 22:46:50 -0400

    added type declaration for Button.Group

---
+++ src/elements/ButtonGroup.d.ts
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+interface Props {
+  className?: string;
+  block?: boolean;
+}
+
+/** A simple button */
+declare const ButtonGroup: React.FunctionComponent<Props>;
+
+export default ButtonGroup;
