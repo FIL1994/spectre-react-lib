aeed4080ce70ebd393630c99b3de34710da87efc
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-09-24 21:23:22 -0400

    updated storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -14,10 +14,7 @@ addParameters({
 
 setAddon(infoAddon);
 
-// automatically import all files ending in *.stories.js
-const req = require.context("../stories", true, /\.stories\.js$/);
-function loadStories() {
-  req.keys().forEach(filename => req(filename));
-}
-
-configure(loadStories, module);
+configure(
+  require.context("../stories", true, /\.stories\.(js|tsx|mdx)$/),
+  module
+);
