05ae0a9ab3206c76bf1df733189622aecb4310d9
Philip Van Raalte <phil.vanraalte@gmail.com>
2023-11-13 19:02:38 -0500

    setup storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,7 @@ module.exports = {
     es2021: true,
     jest: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'prettier',
-    'plugin:storybook/recommended',
-  ],
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'plugin:storybook/recommended', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
