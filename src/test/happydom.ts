c91d491d2b4c5545ff9c9e9fc6825fa4f614f6ff
Philip Van Raalte <phil.vanraalte@gmail.com>
2024-05-27 17:54:55 -0400

    chore: fix bun github actions (#107)
    * chore: fix bun github actions
    
    * change bun version for test
    
    * updated happy-dom
    
    * updated pnpm lock file
    
    * Update test.yml
    
    * updated test action
    
    * cleanup dom after each test

--- src/test/happydom.ts
+++ src/test/happydom.ts
@@ -1,5 +1,9 @@
 // Used to load happy-dom in bun when running tests
-
+import { afterEach } from 'bun:test';
 import { GlobalRegistrator } from '@happy-dom/global-registrator';
 
 GlobalRegistrator.register();
+
+afterEach(() => {
+  document.body.innerHTML = '';
+});
